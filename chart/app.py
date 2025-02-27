117f556784b32ce53d2f15fff8992b9eeeffd887
nuclearpolygon <45406953+nuclearpolygon@users.noreply.github.com>
2025-02-28 01:21:41 +0300

    tst dynamic layout update

--- chart/app.py
+++ chart/app.py
@@ -1,4 +1,4 @@
-from dash import Dash, html, dcc, callback, Output, Input, no_update, State, set_props
+from dash import Dash, html, dcc, callback, Output, Input, no_update, State, set_props, Patch, ALL
 from dash.exceptions import PreventUpdate
 import plotly.express as px
 import plotly.graph_objects as go
@@ -12,53 +12,60 @@ log.addHandler(logging.StreamHandler(sys.stdout))
 log.setLevel(logging.INFO)
 
 engine = sqlalchemy.create_engine("sqlite:////app/financial_data.db", execution_options={"sqlite_raw_colnames": True})
-df = pd.read_sql_table('stock_data', con=engine)
+
+class CandleGraph(dcc.Graph):
+    def __init__(self, table_name):
+        df = pd.read_sql_table(table_name, con=engine)
+        figure = go.Figure(data=[go.Candlestick(
+            x=df['Date'], open=df['Open'], high=df['High'], low=df['Low'], close=df['Close']
+        )])
+        figure.update_layout(xaxis_rangeslider_visible=False)
+        figure.update_yaxes(fixedrange=True)
+        super().__init__(id={'type': 'candle-graph', 'index': table_name}, figure=figure)
 
 class App(Dash):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.figure = go.Figure(data=[go.Candlestick(
-            x=df['Date'], open=df['Open'], high=df['High'], low=df['Low'], close=df['Close']
-        )])
-        self.figure.update_layout(xaxis_rangeslider_visible=False)
-        self.figure.update_yaxes(fixedrange=True)
-        self.graph = dcc.Graph(id='graph-content', figure=self.figure)
+
         self.layout = (
             html.H1(children='Title of Dash App', style={'textAlign':'center'}),
-            # dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
-            self.graph
+            dcc.Dropdown(['btcusd', 'ethusd'], id='dropdown-selection'),
+            html.Div(id="graph-container-div", children=[CandleGraph('stock_data')]),
         )
 
 
 app = App(__name__)
 
 @callback(
-    Output('graph-content', 'figure'),
-    Input('graph-content', 'relayoutData'),
-    State('graph-content', 'figure'),
+    Output('graph-container-div', 'children'),
+    Input({'type': 'candle-graph', 'index': ALL}, 'relayoutData'),
+    State('graph-container-div', 'children'),
     prevent_initial_call=True
 )
-def update_graph(value, figure):
-    f = None
+def update_graph(value, children):
+    c = []
     if not value:
         raise PreventUpdate
-    if 'xaxis.range[0]' in value:
+    if 'xaxis.range[0]' in value[0]:
         with engine.connect() as conn:
             q = text(
                 "SELECT max(High) FROM stock_data "
                 "WHERE Date BETWEEN :r0 AND :r1"
             )
-            _max = conn.execute(q, {'r0': value['xaxis.range[0]'], 'r1': value['xaxis.range[1]']}).scalar_one()
-            log.info(f'MAX VAL: {_max}')
+            r0, r1 = (value[0]['xaxis.range[0]'], value[0]['xaxis.range[1]'])
+            _max = conn.execute(q, {'r0': r0, 'r1': r1}).scalar_one()
+            # log.info(f'MAX VAL: {_max}')
             q = text(
                 "SELECT min(Low) FROM stock_data "
                 "WHERE Date BETWEEN :r0 AND :r1"
             )
-            _min = conn.execute(q, {'r0': value['xaxis.range[0]'], 'r1': value['xaxis.range[1]']}).scalar_one()
-            log.info(f'MIN VAL: {_min}')
-        f = go.Figure(figure)
-        f.update_yaxes({'range': [_min * .999, _max * 1.001], 'autorange': False})
-    return f or figure
+            _min = conn.execute(q, {'r0': r0, 'r1': r1}).scalar_one()
+            # log.info(f'MIN VAL: {_min}')
+        for figure in children:
+            f = go.Figure(figure)
+            f.update_yaxes({'range': [_min * .999, _max * 1.001], 'autorange': False})
+            c.append(f)
+    return c or children
 server = app.server
 if __name__ == '__main__':
     app.run(debug=True)
--- chart/db.py
+++ chart/db.py
@@ -4,13 +4,38 @@ from huobi.utils import *
 import pandas as pd
 import sqlite3
 import time
+import datetime
+
+
+def time_mod(time, delta, epoch=None):
+    if epoch is None:
+        epoch = datetime.datetime(1970, 1, 1, tzinfo=time.tzinfo)
+    return (time - epoch) % delta
+
+def time_floor(time, delta, epoch=None):
+    mod = time_mod(time, delta, epoch)
+    return time - mod
+
+Intervals = {
+    CandlestickInterval.HOUR4: {'hours': 4}
+}
+
+def round_time(_interval):
+    t = datetime.datetime.fromtimestamp(time.time())
+    delta = datetime.timedelta(**Intervals[_interval])
+    epoch = datetime.datetime(1970, 1, 1, tzinfo=t.tzinfo)
+    now = (time_floor(t, delta) - epoch).total_seconds()
+    return now
+
+
 
 market_client = MarketClient()
 interval = CandlestickInterval.HOUR4
+
 symbol = "ethusdt"
 size = 2000
 list_obj = market_client.get_candlestick(symbol, interval, size)
-date = pd.to_datetime(time.time(), unit='s')
+date = pd.to_datetime(round_time(interval), unit='s')
 
 data = {
     'Date': pd.date_range(end=date, periods=size, freq='4h'),
